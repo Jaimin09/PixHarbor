9587bba71423cf7a5be7e630243e1639fa8c39c4
Jaimin Patel <jaiminhpatel09@gmail.com>
2020-12-01 23:00:38 +0530

    Save Images updatte

--- save_image.php
+++ save_image.php
@@ -11,12 +11,17 @@ if (!$connection) {
 }
 
 $image_name = $_POST['image_name'];
-$email=$_POST['email'];
-$folder=(explode('_',$image_name,2))
-$path = "Images/".$folder[0]."/".$image_name.'.jpg';
+$email = "jaimin@gmail.com";
 
 // connect to the database
 $query = "insert into images values('$email','$image_name')";
+
 $res = mysqli_query($connection, $query);
+$folder=(explode('_',$image_name,2));
+$path = "Images/".$folder[0]."/".$image_name;
+echo "<img src = '$path'> ";
+echo "Price : $10";
+
+
 
 ?>
